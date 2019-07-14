5dc1915e8df17963093c58402ff5dafc53f523bb
kartikeya-dewal <kartikeya.dewal@gmail.com>
2019-07-14 23:39:15 +1000

    added routes and models

---
+++ routes/api/profile.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+
+/**
+ * @route       GET /api/profile/me
+ * @description Get current user's profile
+ * @access      Private
+ */
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route       POST /api/profile
+ * @description Create or update a user profile
+ * @access      Private
+ */
+router.post('/', [auth,
+  [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { company, website, location, bio, status,
+    githubUsername, skills, youtube, facebook,
+    twitter, instagram, linkedin } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubUsername) profileFields.githubUsername = githubUsername;
+  if (skills) profileFields.skills = skills.split(',').map(skill => skill.trim());
+
+  // Build social object
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (instagram) profileFields.social.instagram = instagram;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    if (profile) {
+      // Update
+      profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+      return res.json(profile);
+    }
+    profile = new Profile(profileFields);
+    await profile.save();
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+  res.send(profileFields.skills);
+});
+
+/**
+ * @route       GET /api/profile
+ * @description Get all profiles
+ * @access      Public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Errror');
+  }
+});
+
+/**
+ * @route       GET /api/profile/user/:userId
+ * @description Get profile by user id
+ * @access      Public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.userId }).populate('user', ['name', 'avatar']);
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Errror');
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/api', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
