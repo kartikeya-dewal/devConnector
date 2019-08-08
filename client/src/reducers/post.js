7d267f4b03b33161362f1c0356874217d81945bf
kartikeya-dewal <kartikeya.dewal@gmail.com>
2019-08-08 14:58:44 +1000

    Add post functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,9 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST,
+  GET_POST
 } from '../actions/types';
 
 const initialState = {
@@ -18,6 +20,16 @@ export default function(state = initialState, action) {
   switch (type) {
     case GET_POSTS:
       return { ...state, posts: payload, loading: false };
+    case GET_POST:
+      return { ...state, post: payload, loading: false };
+    case ADD_POST:
+      return { ...state, posts: [payload, ...state.posts], loading: false };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
     case POST_ERROR:
       return { ...state, error: payload, loading: false };
     case UPDATE_LIKES:
@@ -26,12 +38,6 @@ export default function(state = initialState, action) {
         posts: state.posts.map(post =>
           post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
-        loading: false
-      };
-    case DELETE_POST:
-      return {
-        ...state,
-        posts: state.posts.filter(post => post._id !== payload),
         loading: false
       };
     default:
