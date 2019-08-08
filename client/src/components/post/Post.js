56d5029dcf25d1bd993d6aca611b73680e4a19c0
kartikeya-dewal <kartikeya.dewal@gmail.com>
2019-08-08 17:53:14 +1000

    add comment functionality

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getPost } from '../../actions/post';
+import Spinner from '../common/Spinner';
+import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost, match.params.id]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
@@ -10,7 +10,8 @@ const PostItem = ({
   post: { _id, name, text, user, avatar, likes, comments, date },
   addLike,
   removeLike,
-  deletePost
+  deletePost,
+  showActions
 }) => {
   return (
     <div className='post bg-white p-1 my-1'>
@@ -25,38 +26,45 @@ const PostItem = ({
         <p className='post-date'>
           Posted on {<Moment format='DD/MM/YYYY'>{date}</Moment>}
         </p>
-        <button
-          onClick={() => addLike(_id)}
-          type='button'
-          className='btn btn-light'>
-          <i className='fas fa-thumbs-up' />{' '}
-          {likes.length > 0 && <span>{likes.length}</span>}
-        </button>
-        <button
-          onClick={() => removeLike(_id)}
-          type='button'
-          className='btn btn-light'>
-          <i className='fas fa-thumbs-down' />
-        </button>
-        <Link to={`/post/${_id}`} className='btn btn-primary'>
-          Discussion{' '}
-          {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            onClick={() => deletePost(_id)}
-            type='button'
-            className='btn btn-danger'>
-            <i className='fas fa-times' />
-          </button>
+        {showActions && (
+          <Fragment>
+            <button
+              onClick={() => addLike(_id)}
+              type='button'
+              className='btn btn-light'>
+              <i className='fas fa-thumbs-up' />{' '}
+              {likes.length > 0 && <span>{likes.length}</span>}
+            </button>
+            <button
+              onClick={() => removeLike(_id)}
+              type='button'
+              className='btn btn-light'>
+              <i className='fas fa-thumbs-down' />
+            </button>
+            <Link to={`/posts/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              {comments.length > 0 && (
+                <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                onClick={() => deletePost(_id)}
+                type='button'
+                className='btn btn-danger'>
+                <i className='fas fa-times' />
+              </button>
+            )}
+          </Fragment>
         )}
       </div>
     </div>
   );
 };
 
+PostItem.defaultProps = {
+  showActions: true
+};
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
